c5a46d4eb165382d19d35e06132d0391cf442b5a
Valiev2002 <70635274+Valiev2002@users.noreply.github.com>
2021-06-27 21:09:16 +0300

    Add files via upload

--- Student_from_Faculty.cpp
+++ Student_from_Faculty.cpp
@@ -1,9 +1,8 @@
 #include "Student_from_Faculty.h"
 #include<iostream>
 double Student_from_Faculty::probability() {
-    if (Student::probability() > 1.0 && Student::probability() < 0.0)
-        std::cout << "Error(я не знаю как по английски,но короче такакя вероятнгсть невозможна)" << std::endl;
-    else {}
+    if (Student::probability()+good_dekan > 1.0 && Student::probability()+ good_dekan < 0.0)
+        std::cout << "Error" << std::endl;
     return Student::probability() + good_dekan;
 }
 Student_from_Faculty::Student_from_Faculty(std::string new_name_of_faculty, double new_good_dekan, std::string new_name,double new_karma, double new_plan) {
