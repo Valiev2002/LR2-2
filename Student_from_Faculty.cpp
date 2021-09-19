0c134188a30c4213c8ab597d35f55427cbe4e078
Valiev2002 <70635274+Valiev2002@users.noreply.github.com>
2021-09-19 23:30:56 +0300

    Add files via upload

--- Student_from_Faculty.cpp
+++ Student_from_Faculty.cpp
@@ -1,9 +1,10 @@
 #include "Student_from_Faculty.h"
 #include<iostream>
 double Student_from_Faculty::probability() {
-    if (Student::probability()+good_dekan > 1.0 && Student::probability()+ good_dekan <  0.0)
-        std::cout << "Error" << std::endl;
-    return Student::probability() * good_dekan;
+    if (Student::probability()+good_dekan < 1.0 && Student::probability()+ good_dekan >  0.0)
+        return Student::probability() * good_dekan;
+    else if(Student::probability()+good_dekan > 1.0){std::cout << "Error: probability can't be more than one" << std::endl;}
+    else if(Student::probability()+good_dekan < 0.0){std::cout << "Error: probability can't be less than zero" << std::endl;}
 }
 Student_from_Faculty::Student_from_Faculty(std::string new_name_of_faculty, double new_good_dekan, std::string new_name,double new_karma, double new_plan) {
     name_of_faculty=new_name_of_faculty;
@@ -16,7 +17,3 @@ void Student_from_Faculty::print() {
     Student::print();
     Faculty::print();
 }
-
-
-
-
